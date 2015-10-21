faf283975d491ab0a253fc550c75874962329db8
Ildebrando <spring5709@caelum-144-03>
2015-10-21 18:50:53 -0200

    Atualizaçao

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/JPAConfiguration.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/JPAConfiguration.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
@@ -15,6 +16,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
+@Configuration
 public class JPAConfiguration {
 
 	@Bean
